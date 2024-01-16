e2837dfd10f1ef52c0646730a6b760255d6a5223
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-01-16 10:22:09 -0300

    save items on localStorage

--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -6,29 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class ListaDeCompraService {
 
-  private listaDeCompra: Item[] = [
-    {
-      "id": 1,
-      "nome": "Queijo prato",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": false
-    },
-    {
-      "id": 2,
-      "nome": "Leite integral",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": false
-    },
-    {
-      "id": 3,
-      "nome": "Mamão papaia",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": true
-    },
-  ]
+  private listaDeCompra: Item[] = []
 
   constructor() {
-    console.log('Instanciando dependências necessárias para o serviço.');
+    this.listaDeCompra = JSON.parse(
+      localStorage.getItem('items') || '[]'
+    )
   }
 
   getListaDeCompra(){
@@ -50,10 +33,16 @@ export class ListaDeCompraService {
   addItemLista(nome: string) {
     const item = this.criarItem(nome)
     this.listaDeCompra.push(item)
+    this.atualizarLocalStorage()
   }
 
   updateTitleItem(item: Item, nome: string) {
     const index = this.listaDeCompra.indexOf(item)
     this.listaDeCompra[index].nome = nome
+    this.atualizarLocalStorage()
+  }
+
+  atualizarLocalStorage() {
+    localStorage.setItem('items', JSON.stringify(this.listaDeCompra));
   }
 }
